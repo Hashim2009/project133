95a263c2e45b911495cc025fd565c3eed9ad809c
Hashim2009 <94174587+Hashim2009@users.noreply.github.com>
2023-08-18 19:16:10 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function bedroom() {
+    window.location = "bedroom.html";
+}
+
+function tvac() {
+    window.location = "tvandac.html";
+}
+
+function diningroom() {
+    window.location = "diningroom.html";
+}
+
+function desk() {
+    window.location = "desk.html";
+}
+
+function fruitbasket() {
+    window.location = "fruitbasket.html";
+}
